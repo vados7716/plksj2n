459baf6c1092ffd9505416cb4b7a7d44a0bf12da
Vindettaffgj <89946720+Vindettaffgj@users.noreply.github.com>
2024-12-24 16:10:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let lastBettingTime = 0;
+let tokenIndex = 0;
+
+const tokens = [
+  "demo", 
+  "demo", 
+  "demo" 
+];
+
+function getAuthorizationToken() {
+  const token = tokens[tokenIndex];
+  tokenIndex = (tokenIndex + 1) % tokens.length;
+  return `Bearer ${token}`;
+}
+
+async function checkSignal() {
+  const url = 'https://crash-gateway-cr.100hp.app/state?id_n=1play_luckyjet';
+  const response = await fetch(url, {
+    headers: {
+      'Authorization': getAuthorizationToken()
+    }
+  });
+  const data = await response.json();
+  const state = data.current_state;
+
+  const luckyJetIcon = document.getElementById('luckyJetIcon');
+  const waitingText = document.getElementById('waitingText');
+  const endingText = document.getElementById('endingText');
+
+  if (state === "betting" && Date.now() - lastBettingTime > 5000) {
+    luckyJetIcon.src = "boy.gif"; 
+    luckyJetIcon.classList.remove('ending');
+    luckyJetIcon.classList.add('flying');
+    waitingText.classList.add('show'); 
+    endingText.classList.remove('show'); 
+    lastBettingTime = Date.now();
+  } else if (state === "ending") {
+    luckyJetIcon.src = "boy.gif"; 
+    luckyJetIcon.classList.remove('flying');
+    luckyJetIcon.classList.add('ending');
+    waitingText.classList.remove('show'); 
+    endingText.classList.add('show');
+  } else if (state === "flying") {
+    luckyJetIcon.src = "boy.gif"; 
+    luckyJetIcon.classList.remove('ending');
+    luckyJetIcon.classList.add('flying');
+    waitingText.classList.add('show'); 
+    endingText.classList.remove('show');
+  } 
+}
+
+setInterval(checkSignal, 1000); // Проверяйте состояние каждые 1 секунду
